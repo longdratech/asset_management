07e24c0e91808721dec89e22a06ba2bc09d2e3e5
Long Tran <LongTH20@fsoft.com.vn>
2023-05-08 15:14:06 +0700

    Update my_booking.dart

--- lib/screens/booking/my_booking.dart
+++ lib/screens/booking/my_booking.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:assets_management/blocs/asset/asset_bloc.dart';
 import 'package:assets_management/blocs/asset/asset_event.dart';
 import 'package:assets_management/blocs/asset/asset_state.dart';
@@ -27,7 +29,7 @@ class MyBooking extends StatefulWidget {
 }
 
 class _MyBookingState extends State<MyBooking> {
-  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  // FirebaseFirestore firestore = FirebaseFirestore.instance;
   late TextEditingController _controller;
   final repository = FirestoreRepository();
   final _bloc = BookingBloc();
@@ -65,197 +67,204 @@ class _MyBookingState extends State<MyBooking> {
       child: BlocProvider(
         create: (context) => _bloc..add(LoadBooking(DateTime.now())),
         child: Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            title: PreferredSize(
-              preferredSize: const Size.fromHeight(30),
-              child: TabBar(
-                isScrollable: true,
-                indicatorColor: Colors.white,
-                onTap: (index) {
-                  _selectedIndex = index;
-                  _bloc.add(LoadBooking(_current(index)));
-                },
-                tabs: datetime.map(
-                  (e) {
-                    return Tab(
-                      child: Text(e),
-                    );
+            appBar: AppBar(
+              centerTitle: true,
+              title: PreferredSize(
+                preferredSize: const Size.fromHeight(30),
+                child: TabBar(
+                  isScrollable: true,
+                  indicatorColor: Colors.white,
+                  onTap: (index) {
+                    _selectedIndex = index;
+                    _bloc.add(LoadBooking(_current(index)));
                   },
-                ).toList(),
+                  tabs: datetime.map(
+                    (e) {
+                      return Tab(
+                        child: Text(e),
+                      );
+                    },
+                  ).toList(),
+                ),
               ),
             ),
-          ),
-          body: RefreshIndicator(
-            onRefresh: () {
-              _bloc.add(LoadBooking(_current(_selectedIndex)));
-              return Future<void>.delayed(const Duration(seconds: 0));
-            },
-            child: BlocBuilder<BookingBloc, BookingState>(
-              builder: (context, state) {
-                if (state is BookingLoading) {
-                  return const Center(child: Text('Loading...'));
-                } else if (state is BookingLoaded) {
-                  final data = state.booking;
-                  if (data.isEmpty) {
-                    return const Center(child: Text('No data!'));
-                  } else {
-                    return Container(
-                      padding: const EdgeInsets.all(20),
-                      child: ListView.builder(
-                        itemCount: data.length,
-                        itemBuilder: (BuildContext context, int i) {
-                          return Padding(
-                            padding: const EdgeInsets.only(top: 12),
-                            child: GestureDetector(
-                              onTap: () {
-                                _showMyDialog(data[i]);
-                              },
-                              child: ListBody(
-                                children: <Widget>[
-                                  BlocBuilder<AssetBloc, AssetState>(
-                                    bloc: _assetBloc
-                                      ..add(
-                                        LoadAssetById(data[i].assetRef),
-                                      ),
-                                    builder: (context, state) {
-                                      if (state is AssetByLoaded) {
-                                        final assetCode = state.asset.assetCode;
-                                        return Text.rich(
-                                          TextSpan(
-                                            text: 'Asset code: ',
-                                            children: [
-                                              TextSpan(
-                                                text: assetCode,
-                                                style: const TextStyle(
-                                                  color: Colors.black,
-                                                  fontWeight: FontWeight.bold,
+            body: RefreshIndicator(
+              onRefresh: () {
+                _bloc.add(LoadBooking(_current(_selectedIndex)));
+                return Future<void>.delayed(const Duration(seconds: 0));
+              },
+              child: BlocBuilder<BookingBloc, BookingState>(
+                builder: (context, state) {
+                  if (state is BookingLoading) {
+                    return const Center(child: Text('Loading...'));
+                  } else if (state is BookingLoaded) {
+                    final data = state.booking;
+                    if (data.isEmpty) {
+                      return const Center(child: Text('No data!'));
+                    } else {
+                      return Container(
+                        padding: const EdgeInsets.all(20),
+                        child: ListView.builder(
+                          itemCount: data.length,
+                          itemBuilder: (BuildContext context, int i) {
+                            return Padding(
+                              padding: const EdgeInsets.only(top: 12),
+                              child: GestureDetector(
+                                onTap: () {
+                                  _showMyDialog(data[i]);
+                                },
+                                child: ListBody(
+                                  children: <Widget>[
+                                    BlocBuilder<AssetBloc, AssetState>(
+                                      bloc: _assetBloc
+                                        ..add(
+                                          LoadAssetById(data[i].assetRef),
+                                        ),
+                                      builder: (context, state) {
+                                        if (state is AssetByLoaded) {
+                                          final assetCode =
+                                              state.asset.assetCode;
+                                          return Text.rich(
+                                            TextSpan(
+                                              text: 'Asset code: ',
+                                              children: [
+                                                TextSpan(
+                                                  text: assetCode,
+                                                  style: const TextStyle(
+                                                    color: Colors.black,
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
                                                 ),
-                                              ),
-                                            ],
-                                          ),
-                                        );
-                                      }
-                                      return const Text('Loading...');
-                                    },
-                                  ),
-                                  Text.rich(
-                                    TextSpan(
-                                      text: 'Trạng thái: ',
-                                      children: [
-                                        data[i].endedAt == null
-                                            ? const TextSpan(
-                                                text: 'Đang mượn',
-                                                style: TextStyle(
-                                                  color: Colors.red,
-                                                  fontWeight: FontWeight.bold,
-                                                ),
-                                              )
-                                            : TextSpan(
-                                                text:
-                                                    'Đã trả (${DateFormat('dd/MM/yyyy - HH:mm').format(data[i].endedAt!)})',
-                                                style: const TextStyle(
-                                                  color: Colors.green,
-                                                  fontWeight: FontWeight.bold,
-                                                ),
-                                              ),
-                                      ],
+                                              ],
+                                            ),
+                                          );
+                                        }
+                                        return const Text('Loading...');
+                                      },
                                     ),
-                                  ),
-                                  Text(
-                                    "Thời gian mượn: ${DateFormat('dd/MM/yyyy - HH:mm').format(data[i].createdAt)}",
-                                  ),
-                                  Text("Người mượn:  ${data[i].employee}"),
-                                ],
+                                    Text.rich(
+                                      TextSpan(
+                                        text: 'Trạng thái: ',
+                                        children: [
+                                          data[i].endedAt == null
+                                              ? const TextSpan(
+                                                  text: 'Đang mượn',
+                                                  style: TextStyle(
+                                                    color: Colors.red,
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
+                                                )
+                                              : TextSpan(
+                                                  text:
+                                                      'Đã trả (${DateFormat('dd/MM/yyyy - HH:mm').format(data[i].endedAt!)})',
+                                                  style: const TextStyle(
+                                                    color: Colors.green,
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
+                                                ),
+                                        ],
+                                      ),
+                                    ),
+                                    Text(
+                                      "Thời gian mượn: ${DateFormat('dd/MM/yyyy - HH:mm').format(data[i].createdAt)}",
+                                    ),
+                                    Text("Người mượn:  ${data[i].employee}"),
+                                  ],
+                                ),
                               ),
-                            ),
-                          );
-                        },
+                            );
+                          },
+                        ),
+                      );
+                    }
+                  } else {
+                    return const Center(
+                      child: Text(
+                        'Đã có lỗi xảy ra. Vui lòng liên hệ LongTH20!',
                       ),
                     );
                   }
-                } else {
-                  return const Center(
-                    child: Text(
-                      'Đã có lỗi xảy ra. Vui lòng liên hệ LongTH20!',
-                    ),
-                  );
-                }
-              },
-            ),
-          ),
-          floatingActionButton: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 10),
-                child: FloatingActionButton(
-                  heroTag: "text",
-                  onPressed: () async {
-                    await showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: const Text('Nhập mã tài sản'),
-                          content: TextField(
-                            controller: _controller,
-                          ),
-                          actions: <Widget>[
-                            TextButton(
-                              style: TextButton.styleFrom(
-                                textStyle:
-                                    Theme.of(context).textTheme.labelLarge,
-                              ),
-                              child: const Text('Cancel'),
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                            ),
-                            TextButton(
-                              style: TextButton.styleFrom(
-                                textStyle:
-                                    Theme.of(context).textTheme.labelLarge,
-                              ),
-                              child: const Text('Xác nhận'),
-                              onPressed: () async {
-                                final assetCode = _controller.text;
-                                if (assetCode.isNotEmpty) {
-                                  _process(_controller.text);
-                                }
-                                _controller.clear();
-                                Navigator.of(context).pop();
-                              },
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  },
-                  child: const Icon(Icons.text_fields),
-                ),
-              ),
-              FloatingActionButton(
-                heroTag: "camera",
-                onPressed: () async {
-                  try {
-                    String assetCode = await FlutterBarcodeScanner.scanBarcode(
-                      '#ff6666',
-                      'Cancel',
-                      true,
-                      ScanMode.QR,
-                    );
-                    if (assetCode != "-1") {
-                      await _process(assetCode);
-                    }
-                  } on PlatformException {
-                    print('failure scan');
-                  }
                 },
-                child: const Icon(Icons.camera_alt_outlined),
               ),
-            ],
-          ),
-        ),
+            ),
+            floatingActionButton: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: FloatingActionButton(
+                    heroTag: "text",
+                    onPressed: () async {
+                      if (!(Platform.isAndroid || Platform.isIOS)) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text('Please use smart phone')));
+                      } else {
+                        await showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: const Text('Nhập mã tài sản'),
+                              content: TextField(
+                                controller: _controller,
+                              ),
+                              actions: <Widget>[
+                                TextButton(
+                                  style: TextButton.styleFrom(
+                                    textStyle:
+                                        Theme.of(context).textTheme.labelLarge,
+                                  ),
+                                  child: const Text('Cancel'),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                                TextButton(
+                                  style: TextButton.styleFrom(
+                                    textStyle:
+                                        Theme.of(context).textTheme.labelLarge,
+                                  ),
+                                  child: const Text('Xác nhận'),
+                                  onPressed: () async {
+                                    final assetCode = _controller.text;
+                                    if (assetCode.isNotEmpty) {
+                                      _process(_controller.text);
+                                    }
+                                    _controller.clear();
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      }
+                    },
+                    child: const Icon(Icons.text_fields),
+                  ),
+                ),
+                FloatingActionButton(
+                  heroTag: "camera",
+                  onPressed: () async {
+                    try {
+                      String assetCode =
+                          await FlutterBarcodeScanner.scanBarcode(
+                        '#ff6666',
+                        'Cancel',
+                        true,
+                        ScanMode.QR,
+                      );
+                      if (assetCode != "-1") {
+                        await _process(assetCode);
+                      }
+                    } on PlatformException {
+                      print('failure scan');
+                    }
+                  },
+                  child: const Icon(Icons.camera_alt_outlined),
+                ),
+              ],
+            )),
       ),
     );
   }
@@ -393,9 +402,7 @@ class _MyBookingState extends State<MyBooking> {
             TextButton(
               child: const Text('Xác nhận'),
               onPressed: () {
-                firestore.collection('booking').doc(booking.id).update({
-                  "endedAt": DateTime.now(),
-                });
+                _bloc.add(ReturnBooking(booking.id, endedAt: DateTime.now()));
                 Navigator.of(context).pop();
               },
             ),
